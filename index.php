b7f6fce0e7d59cf9c79b189588c9ba0ed2ee796a
FaxriddinDev <faxriddinboboyev1129@gmail.com>
2022-04-13 14:39:31 +0500

    update Two

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+$a  = 10;
+
+// switch ($a) {
+
+// 	case 1: echo "Birrinchi Yacheyka"; break;
+// 	case 2: echo "Ikkiinchi Yacheyka"; break;
+// 	case 3: echo "Uchunchi Yacheyka"; break;
+// 	case 4: echo "To`rtinchi Yacheyka"; break;
+// 	case 5: echo "Beshinchi Yacheyka"; break;
+// 	case 6: echo "Oltinchi Yacheyka"; break;
+// 	case 7: echo "Yettinchi Yacheyka"; break;
+
+// 	default: echo "Bunday Yacheyka Mavjudmas"; break;
+// }
+
+// $s = 1;
+// $k = 4;
+
+// echo $s++;
+// echo $k;
+
+$i = 0;
+// while (20 >= $i) {
+// 	echo $i.'  ';
+// 	$i++;
+// }
+
+// do{
+// 	$i++;
+// 	echo $i;
+// }
+// while (10 > $i);
+
+
