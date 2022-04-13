5ec90c8bee241277899e7195b2b9532491e70af9
FaxriddinDev <faxriddinboboyev1129@gmail.com>
2022-04-13 15:59:03 +0500

    update three

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 
-$a  = 10;
-
+// $a  = 100000000000000;
 // switch ($a) {
-
 // 	case 1: echo "Birrinchi Yacheyka"; break;
 // 	case 2: echo "Ikkiinchi Yacheyka"; break;
 // 	case 3: echo "Uchunchi Yacheyka"; break;
@@ -21,7 +19,7 @@ $a  = 10;
 // echo $s++;
 // echo $k;
 
-$i = 0;
+// $i = 0;
 // while (20 >= $i) {
 // 	echo $i.'  ';
 // 	$i++;
@@ -33,4 +31,55 @@ $i = 0;
 // }
 // while (10 > $i);
 
+// for ($i = 0; $a > $i; $i++) { 
+// 	if ($i <= 100) {
+// 		echo $i.'  ';
+// 	}
+// 	if ($i > 100) {
+// 		break;
+// 	}
+// }
 
+// Year
+// month
+// day
+
+// Hour
+// minute
+// second
+
+// date_default_timezone_set('America/Los_Angeles');
+// $time = date('Y-m-d H:m:s');
+// $timestemp = strtotime($time);
+
+// echo 'DATETIME = '.date('Y-m-d H:m:s');
+// echo "<br>";
+// echo 'DATE = '.date('Y-m-d');
+// echo "<br>";
+// echo 'TIMESTEMP = '.$timestemp;
+
+// echo "<br>";
+
+// $newTime = $timestemp + 86400;
+// echo 'timestemp + 86400 = ' .date('Y-m-d H:m:s', $newTime);
+
+
+
+// MASALALAR:
+
+/*
+
+1. berilgan n songacha bulgan sonlarni yig'indisi;
+2. berilgan n songacha juft sonlar yigindis;
+3. berilgan n songacha top sonlar yigindisi;
+4. berilgan n songacha tuplarini chiqarish;
+5. berilgan n songacha tup sonlarni yig'indsi;
+6. oylarni chiqaruvchi dastur 2 kiritilsa dekabr;
+7. hafta kunlarni chiqaruvchi  topish
+8. N songacha sonlar ko'paytmasi;
+9. Sonni nechchi xonali ekanligini topuvchi dastur.
+10. Ikki xonali Raqamlar yigindisi
+11. Uch xonali Raqamlar yigindisi
+12.Ixtiyoriy xonali Raqamlar yigindisi
+
+*/
